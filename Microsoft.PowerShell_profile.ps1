d84a81639d81b99f0c52ccb8a65d5a0e3a2325fe
Darrin Britton <brittod@wwu.edu>
2018-10-09 07:50:25 -0700

    Removed legacy posh-git code from profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,34 +1,31 @@
-#$env:path += ";" + (Get-Item "Env:ProgramFiles").Value + "\Git\bin"
-
-# Load posh-git example profile
-. 'C:\Users\brittod\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
-
-set-alias vim "C:/Program Files (x86)/Vim/Vim80/./vim.exe"
-
 # To edit the Powershell Profile
 # (Not that I'll remember this)
-Function Edit-Profile {
-
+Function Edit-Profile
+{
     vim $profile
-
 }
 
 # To edit Vim settings
-Function Edit-Vimrc {
-
+Function Edit-Vimrc
+{
     vim $HOME\_vimrc
-
 }
 
-function subl {
+function subl { 
 
-    &"${Env:ProgramFiles}\Sublime Text 3\sublime_text.exe" $args
+    &"${Env:ProgramFiles}\Sublime Text 3\sublime_text.exe" $args 
 
 }
-function code {
+function code { 
 
     &"${ENV:ProgramFiles}\Microsoft VS CODE\code.exe" $args
 
+}
+
+Function Python {
+
+    &"C:\Python27\python.exe" $args
+    
 }
 
 . "C:\Users\brittod\documents\windowspowershell\Get-ChildItem-Color.ps1"
@@ -36,7 +33,11 @@ function code {
 Set-Alias ls Get-ChildItem-Color -option AllScope
 #Set-Alias ls Get-ChildItem-Format-Wide -option AllScope
 
-$VIMpath = "$ENV:HomeDrive\Program Files (x86)\Vim\vim80\vim.exe"
+$VIMpath = "$ENV:HomeDrive\Program Files (x86)\Vim\vim81\vim.exe"
 Set-alias vim $VIMpath
+
+Set-Alias 'reboot' Restart-Computer -Force
+
+Set-Alias 'Shutdown' Stop-Computer -Force
 
 Get-MOTD
